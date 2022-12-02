49a096e63861ffc9edbaad42ba54bcf26257b81f
EddTorres <30393680+EddTorres@users.noreply.github.com>
2022-12-01 23:19:29 -0600

    Update Form1.vb
    Se agrego la mascara para lectura de datos de DUI con el guin y la cantidad de caracteres.

--- Form1.vb
+++ Form1.vb
@@ -54,7 +54,8 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
-
+    
+    
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles datosAlumno.CellContentClick
 
     End Sub
